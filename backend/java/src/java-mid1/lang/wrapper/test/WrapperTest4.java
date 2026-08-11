ba55b04ba2387bc167b4a31f33a6b5bf33b70751
godness999 <godness999@naver.com>
2026-08-11 17:02:38 +0900

    260811 - Wrapper, Class, System, Math, Random 클래스 학습

---
+++ backend/java/src/java-mid1/lang/wrapper/test/WrapperTest4.java
@@ -0,0 +1,27 @@
+package lang.wrapper.test;
+
+/*
+문제 3.
+String str을 Integer로 변환해서 출력해라.
+Integer를 int로 변환해서 출력해라.
+int를 Integer로 변환해서 출력해라.
+오토 박싱, 오토 언박싱을 사용해서 변환해야 한다.
+ */
+public class WrapperTest4 {
+
+    public static void main(String[] args) {
+        String str = "100";
+
+        //String -> Integer
+        Integer integer1 = Integer.valueOf(str);
+        System.out.println("integer1 = " + integer1);
+
+        //Integer -> int
+        int intValue = integer1;
+        System.out.println("intValue = " + intValue);
+
+        //int -> Integer
+        Integer integer2 = intValue;
+        System.out.println("integer2 = " + integer2);
+    }
+}
